1d91345d92bbb6eaef8222ce23a99fcb61478582
Daniel L O'Malley <omalley1@mit.edu>
2012-02-05 17:56:28 -0500

    updated urls.py for movies

--- dormbase/urls.py
+++ dormbase/urls.py
@@ -10,6 +10,7 @@ urlpatterns = patterns('',
     url(r'^$', 'dormbase.views.home', name='home'),
     url(r'^directory$', 'dormbase.core.views.directory', name='directory'),
     url(r'^directory_json$', 'dormbase.core.views.directory_json', name='directory_json'),
+    url(r'^movies$', 'dormbase.movie.views.list_movies', name='directory_json'),
 
     url(r'^media/(?P<path>.*)$', 'django.views.static.serve', {
              'document_root': settings.MEDIA_ROOT,
