727656ee861c1473a6f9b68ad37a600b46c80648
Alexander Chernyakhovsky <achernya@mit.edu>
2012-02-18 14:33:43 -0500

    Simple laundryview poller

---
+++ dormbase/data/laundry.py
@@ -0,0 +1,15 @@
+import lxml.html
+
+def get_machines():
+    lvs = lxml.html.parse('http://laundryview.com/lvs.php')
+    div = lvs.find(".//div[@id='campus1']")
+    rooms = []
+    status = []
+    for a in div.findall('.//a'):
+        rooms.append(str(a.text).strip().title())
+    for span in div.findall('.//span'):
+        status.append(str(span.text).strip())
+    return zip(rooms, status)
+
+print get_machines()
+
