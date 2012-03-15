3689209d309e2a1917928906eceb2ca730dda43a
Daniel OMalley <omalley1@mit.edu>
2012-03-15 16:53:12 -0400

    packages: moving package app to dashboard.

--- dormbase/package/views.py
+++ dormbase/package/views.py
@@ -14,7 +14,7 @@ def desk_worker(request):
     packages = []
     bins = ['A', 'B', 'C', 'D', 'Floor']
 
-    for i in xrange(1, random.randint(4,10)):
+    for i in xrange(1, random.randint(30,100)):
         u = TestUser()
         u.username += str(i)
         print u.username
