86fd468099502a81aed95761816ec561118561f2
Luke O'Malley <omalley1@mit.edu>
2012-03-17 10:32:52 -0400

    dashboard: refined html and added alert and package scan demo

--- dormbase/populate.py
+++ dormbase/populate.py
@@ -1,9 +1,9 @@
 from dormbase.core.populate import *
 from dormbase.package.populate import *
-# from dormbase.movie.populate import *
+from dormbase.movie.populate import *
 
 def import_test_database():
     import_test_directory()
     import_test_packages()
-#    import_test_films()
+    import_test_films()
     
