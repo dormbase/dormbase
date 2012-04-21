0f997f44698ab117086be55fb822a3cb9bbbbe56
Alexander Chernyakhovsky <achernya@mit.edu>
2012-04-21 00:10:59 -0400

    Clean up stale parts of core/search_indexes

--- dormbase/core/search_indexes.py
+++ dormbase/core/search_indexes.py
@@ -4,9 +4,6 @@ from dormbase.core.models import *
 
 class ResidentIndex(indexes.SearchIndex):
     text = indexes.EdgeNgramField(document=True, use_template=True)
-    # We add this for autocomplete.
-    #athena_username = indexes.EdgeNgramField(model_attr='athena')
-    #first_name = indexes.EdgeNgramField(model_attr='user__first_name')
     
     def get_model(self):
         return Resident
