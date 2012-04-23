66a8b21c0e37c1c90b7e650a40953c9324aab290
Drew Dennison <drewdennison@gmail.com>
2012-04-23 05:33:05 -0400

    Added Dining Menu RSS feed. Still needs to be stylized

--- dormbase/views.py
+++ dormbase/views.py
@@ -2,6 +2,11 @@ from django.shortcuts import render_to_response
 from django.contrib import auth
 from django.contrib.auth.decorators import login_required
 from django.template import RequestContext
+import feedparser
+
+
 
 def home(request):
-    return render_to_response('index.html', context_instance = RequestContext(request))
+    d = feedparser.parse('http://www.cafebonappetit.com/rss/menu/402') #should get cached and refreshed with a cron job
+    payload = {'menu': d.entries[0].description}
+    return render_to_response('index.html', payload, context_instance = RequestContext(request))
