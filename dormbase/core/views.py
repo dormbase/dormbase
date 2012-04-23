9dfd8bcd5728405cbe69e74c06442ad3bb255bbd
Alexander Chernyakhovsky <achernya@mit.edu>
2012-04-23 17:16:33 -0400

    Remove unneeded code from previous engine

--- dormbase/core/views.py
+++ dormbase/core/views.py
@@ -13,34 +13,6 @@ from haystack.query import SearchQuerySet
 import json
 import itertools
 
-def user_to_dict(user):
-    try:
-        room, year = user.get_profile().room, user.get_profile().year
-    except Resident.DoesNotExist:
-        room, year = 'n/a', 'n/a'
-
-    return {
-        'id' : user.id,
-        'username' : user.username,
-        'firstname' : user.first_name,
-        'lastname' : user.last_name,
-        'room' : str(room), # force room to be serialized
-        'year' : str(year),
-    }
-
-def resident_to_dict(resident):
-    return {
-        'id' : resident.user.id,
-        'username' : resident.user.username,
-        'firstname' : resident.user.first_name,
-        'lastname' : resident.user.last_name,
-        'room' : str(resident.room), # force room to be serialized
-        'year' : str(resident.year),
-    }
-
-def room_to_dict(room):
-    return map(resident_to_dict, Resident.objects.select_related().filter(room__id = room.id))
-
 def directory(request):
     return render_to_response('core/directory.html', context_instance = RequestContext(request))
 
