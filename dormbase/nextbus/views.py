751ba35fb143e8bb6c7fa6bbe1f8c7479596fb47
Luke O'Malley <omalley1@mit.edu>
2012-05-01 22:46:08 -0400

    nextbus: added nextbus page

--- dormbase/nextbus/views.py
+++ dormbase/nextbus/views.py
@@ -1,15 +1,33 @@
-from django.http import HttpResponse
+# Dormbase -- open-source dormitory database system
+# Copyright (C) 2012 Alex Chernyakhovsky <achernya@mit.edu>
+#                    Drew Dennison       <dennison@mit.edu>
+#                    Isaac Evans         <ine@mit.edu>
+#                    Luke O'Malley       <omalley1@mit.edu>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+from django.shortcuts import render_to_response, RequestContext
 import lxml.etree
 import datetime
-import json
 
 # Info on Nextbus api:
 # http://www.nextbus.com/xmlFeedDocs/NextBusXMLFeed.pdf
 
-baseNextBusURL = 'http://webservices.nextbus.com/service/publicXMLFeed?command=predictions&'
-
 def nextbus(request):
+    baseNextBusURL = 'http://webservices.nextbus.com/service/publicXMLFeed?command=predictions&'
     agency = 'mit'
+    stop = 'simmhl'
 
     now = datetime.datetime.now()
     tech_start = now.replace(hour=6,  minute=15, second=0) 
@@ -17,14 +35,12 @@ def nextbus(request):
 
     if  now > tech_start and now < tech_end:
         route = 'tech'
-        stop = 'simmhl'
-
     else:
         route = 'saferidecambwest'
-        stop = 'simmhl'
-    
+
     data = lxml.etree.parse(baseNextBusURL + 'a=' + agency + '&r=' + route + '&s=' + stop)
     next_times = [prediction.get('minutes') for prediction in data.findall('.//predictions/direction/prediction')]
-    jsonout = json.dumps(next_times, sort_keys=True, indent=4)
-    print jsonout
-    return HttpResponse(jsonout, mimetype="application/json")
+
+    payload = {'times': next_times}
+    
+    return render_to_response('nextbus/nextbus.html', payload, context_instance = RequestContext(request))
