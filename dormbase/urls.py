4e4d076ce28f1ff04987c244c2c9ac2eed212520
Quentin Smith <quentin@mit.edu>
2013-03-02 21:40:40 -0500

    Allow guest signins with autocomplete
    This adds a dependency on django-autocomplete-light

--- dormbase/urls.py
+++ dormbase/urls.py
@@ -21,6 +21,9 @@ from django.conf.urls.defaults import patterns, include, url
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 import settings
 
+import autocomplete_light
+autocomplete_light.autodiscover()
+
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 admin.autodiscover()
@@ -79,6 +82,8 @@ urlpatterns = patterns('',
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
+
+    url(r'autocomplete/', include('autocomplete_light.urls')),
                        
 )
 urlpatterns += staticfiles_urlpatterns()
